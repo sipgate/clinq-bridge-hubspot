d79099946b7a6f1fa80ea8951b5e393a1ecc31b3
Felix Gladisch <felix@felixgladisch.de>
2018-07-31 17:32:43 +0200

    fix: contact parsing

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const Clinq = require("@clinq/bridge");
+const clinq = require("@clinq/bridge");
 const ClinqLoader = require("./lib/clinq-loader");
 const ClinqAdapter = require("./lib/adapters/clinq-adapter-hubspot");
 const clinqLoader = new ClinqLoader(ClinqAdapter);
 
 const adapter = {
 	getContacts: async ({ apiKey, apiUrl }) => {
-		const contacts = await clinqLoader.fetchContacts(apiKey, apiUrl);
-		console.log(contacts);
-		return contacts;
+		try {
+			const contacts = await clinqLoader.fetchContacts(apiKey, apiUrl);
+			return contacts;
+		} catch (error) {
+			clinq.unauthorized();
+		}
 	}
 };
 
-Clinq.start(adapter);
+clinq.start(adapter);
