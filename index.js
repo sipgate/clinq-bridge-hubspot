21588234e99414c00b500bf338611f4112d46468
Benjamin Kluck <kluck@sipgate.de>
2018-09-25 17:50:37 +0200

    fix: Do not filter contacts without name

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const clinq = require("@clinq/bridge");
 const { getContacts } = require("./lib/clinq-loader");
 
 const adapter = {
-	getContacts: async ({ apiKey, apiUrl }) => {
-		return await getContacts(apiKey, apiUrl);
-	}
+  getContacts: async ({ apiKey, apiUrl }) => {
+    return await getContacts(apiKey, apiUrl);
+  }
 };
 
 clinq.start(adapter);
