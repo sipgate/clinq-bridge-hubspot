4b2ba8d4649e0272668727fd98c7971f8a2594be
Pascal Jordin <pascal@jordin.eu>
2019-01-24 16:43:24 +0100

    feat: Upgrade hubspot/bridge dep to latest and add create contact functionality

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const clinq = require("@clinq/bridge");
-const { getContacts } = require("./lib/clinq-loader");
+const { getContacts, createContact } = require("./lib/clinq-loader");
 
 const adapter = {
   getContacts: async ({ apiKey, apiUrl }) => {
     return await getContacts(apiKey, apiUrl);
+  },
+  createContact: async ({ apiKey, apiUrl }, contact) => {
+    return await createContact(apiKey, contact);
   }
 };
 
